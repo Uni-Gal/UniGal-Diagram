597e528ef0a75824959930b983958c98939a1729
LaoshuBaby <keaitianxinmail@qq.com>
2021-01-10 22:11:09 +0800

    Modify SVG more unigalish and fix frontend

--- Render/UniGal-DiagramRender_Frontend.cpp
+++ Render/UniGal-DiagramRender_Frontend.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include <fstream>
+#include <string>
 
 using namespace std;
 
@@ -25,6 +26,7 @@ string spaceLevel(int num)
 			{
 				polySpaceLevel = polySpaceLevel + singalSpaceLevel;
 			}
+			return polySpaceLevel;
 		}
 	}
 	else
@@ -38,13 +40,20 @@ int main(void)
 	fstream IO_FILE;
 	IO_FILE.open("simple.svg", ios::out);
 	
+	int deepth = 0;
+
 	//HEADER
+	deepth += 1;
 	IO_FILE << "<?xml version=\"1.0\" standalone=\"no\"?>" << endl << endl;
+	deepth -= 1;
+
+	deepth += 1;
 	IO_FILE << "<!";
 	IO_FILE << "DOCTYPE svg PUBLIC" << endl;
-	IO_FILE << "  \" -//W3C//DTD SVG 1.1//EN\"" << endl;
-	IO_FILE << "  \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\"" << endl;
-	IO_FILE << ">";// << endl;
+	IO_FILE << spaceLevel(deepth) << "\" -//W3C//DTD SVG 1.1//EN\"" << endl;
+	IO_FILE << spaceLevel(deepth) << "\"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\"" << endl;
+	IO_FILE << ">" << endl;
+	deepth -= 1;
 
 	//NODELIST
 	IO_FILE << "<!--" << endl << endl;
@@ -55,11 +64,19 @@ int main(void)
 	//SVG
 
 	////SVGHEADER
-	IO_FILE << "<!--" << endl << endl;
+	deepth += 1;
+	IO_FILE << "<svg" << endl;
+	IO_FILE << spaceLevel(deepth) << "viewBox=" <</*argv(viewBox)*/"" << endl;
+	IO_FILE << spaceLevel(deepth) << "xmlns=" <</*static string*/"" << endl;
+	IO_FILE << spaceLevel(deepth) << "version=" <</*static string*/"" << endl;
+	IO_FILE << ">" << endl;
+	deepth -= 1;
 	////SVGCONTENT
+	deepth += 1;
+	deepth -= 1;
 
 	////SVGTAIL
 	IO_FILE << "</svg>" << endl << endl;
-
+	
 	return 0;
 }
