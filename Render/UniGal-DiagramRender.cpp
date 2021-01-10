b639247e98b720a7d7b945a9d2fe3785099e9ee5
快乐的老鼠宝宝 <42690037+LaoshuBaby@users.noreply.github.com>
2021-01-11 03:38:23 +0800

    using main to hock be and fe with two "main"

---
+++ Render/UniGal-DiagramRender.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <vector>
+
+//半夜来灵感了电脑没电了，手机打字在github网页版写代码一个文件一commit
+//惨的很
+
+#include "前端.cpp"
+#include "后端.cpp"
+
+#include "Point.h"
+#include "Arrow.h"
+#include ""
+
+//没有IDE的情况下纯手机输入法打记事本真困难呢
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+//傻逼网页编辑器没有缩进，下次记得删掉
+main_Backend();
+main_Frontend();
+return 0;
+}
